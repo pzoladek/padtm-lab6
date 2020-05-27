7ab219da6816d4bddc96a94ca3c8c44ec7d5f2fd
Pawel Zoladek <pawel.zoladek@intive.com>
2020-05-27 18:21:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/padtm_lab6/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.padtm_lab6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gameList(View v) {
+        Intent intencja = new Intent( getApplicationContext(), GamesList.class);
+        intencja.putExtra("gra", v.getId());
+        startActivity(intencja);
+    }
+}
